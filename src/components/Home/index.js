8b5d75bb571482b5d6f76ff768b9c9705fe4f168
kubal97 <jakubl1997@interia.pl>
2019-10-25 20:35:48 +0200

    Configuration commit

---
+++ src/components/Home/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+// eslint-disable-next-line require-jsdoc
+class Home extends React.Component {
+// eslint-disable-next-line require-jsdoc
+  render() {
+    return <div>Home component</div>;
+  }
+}
+export default Home;
