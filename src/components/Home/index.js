65293c9365f7f1636251c3f6ba834948199ad657
kubal97 <jakubl1997@interia.pl>
2019-10-26 18:12:28 +0200

    Add homepage

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import './styles.scss';
+import arrowRight from '../../assets/arrow-right.png';
 // eslint-disable-next-line require-jsdoc
 class Home extends React.Component {
 // eslint-disable-next-line require-jsdoc
   render() {
-    return <div>Home component</div>;
+    return (
+      <div className="home">
+        <div className="container">
+          <h3 className="container__title">explore the dark side of...</h3>
+          <h1 className="container__star">star wars</h1>
+          <h4 className="container__description">A long, long time ago in a
+              distant galaxy, Star Wars has redefined the blockbuster movies ...
+              We come to the rescue to remind you of the details
+              of individual heroes, planets and even vehicles and
+              many other curiosities</h4>
+          <div className="container__explore">
+            <p className="container__explore--title">Explore</p>
+            <button className="container__explore--button">
+              <img
+                className="arrow"
+                src={arrowRight}
+                alt="Arrow right"/>
+            </button>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 export default Home;
---
+++ src/components/Nav/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.scss';
+// eslint-disable-next-line require-jsdoc
+class Nav extends React.Component {
+  // eslint-disable-next-line require-jsdoc
+  render() {
+    return (<div className="nav">Nav</div>);
+  }
+}
+export default Nav;
