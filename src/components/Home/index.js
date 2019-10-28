b1b05c0edae0e648de62c189f549908f5e15a2c7
kubal97 <jakubl1997@interia.pl>
2019-10-28 19:17:35 +0100

    Add navigation design

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import './styles.scss';
 import arrowRight from '../../assets/arrow-right.png';
+import Nav from '../Nav';
 // eslint-disable-next-line require-jsdoc
 class Home extends React.Component {
 // eslint-disable-next-line require-jsdoc
   render() {
     return (
       <div className="home">
+        <Nav />
         <div className="container">
           <h3 className="container__title">explore the dark side of...</h3>
           <h1 className="container__star">star wars</h1>
           <h4 className="container__description">A long, long time ago in a
               distant galaxy, Star Wars has redefined the blockbuster movies ...
               We come to the rescue to remind you of the details
-              of individual heroes, planets and even vehicles and
+              of individual heroes, planets, vehicles and
               many other curiosities</h4>
           <div className="container__explore">
             <p className="container__explore--title">Explore</p>
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,10 +1,41 @@
 import React from 'react';
+import Home from '../Home';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
 import './styles.scss';
 // eslint-disable-next-line require-jsdoc
 class Nav extends React.Component {
   // eslint-disable-next-line require-jsdoc
   render() {
-    return (<div className="nav">Nav</div>);
+    return (
+      <Router>
+        <div>
+          <nav className="nav">
+            <ul className="ul">
+              <li><Link className="item" >Films</Link></li>
+              <li><Link className="item" >People</Link></li>
+              <li><Link className="item" >Planets</Link></li>
+              <Link className="logo" to="/">
+                <div className="logo__upper">star wars</div>
+                <div className="logo__lower">wiki</div>
+              </Link>
+              <li><Link className="item" >Species</Link></li>
+              <li><Link className="item" >Starships</Link></li>
+              <li><Link className="item">Vehicles</Link></li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
   }
 }
 export default Nav;
