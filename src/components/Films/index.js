a15b2bd9672d55b69d27aea41fb5dfffd4f3b969
kubal97 <jakubl1997@interia.pl>
2019-11-14 00:22:01 +0100

    Fix images in films

--- src/components/Films/index.js
+++ src/components/Films/index.js
@@ -1,20 +1,39 @@
 import React from 'react';
 import axios from 'axios';
+// import moment from 'moment';
 import './styles.scss';
-import arrowYellow from '../../assets/arrow-yellow.png';
+import First from '../../assets/1.png';
+import Second from '../../assets/2.jpg';
+import Third from '../../assets/3.jpg';
+import Fourth from '../../assets/4.png';
+import Fifth from '../../assets/5.png';
+import Sixth from '../../assets/6.png';
+import Seventh from '../../assets/7.jpg';
+import Loader from '../../assets/loader.png';
+
+const images = [First, Second, Third, Fourth, Fifth, Sixth, Seventh];
 // eslint-disable-next-line require-jsdoc
 class Films extends React.Component {
   // eslint-disable-next-line require-jsdoc
   constructor(props) {
     super(props);
-    this.state = {isLoaded: false, films: []};
+    this.state = {
+      loading: true,
+      isLoaded: false,
+      films: [],
+    };
   }
   // eslint-disable-next-line require-jsdoc
   onLoadFilms() {
-    axios.get('https://swapi.co/api/films/').then((response) => {
-      this.setState({isLoaded: true, films: response.data.results});
-      console.log(this.state.films);
-    });
+    axios.get('https://swapi.co/api/films/')
+        .then((response) => {
+          this.setState({
+            loading: false,
+            isLoaded: true,
+            films: response.data.results,
+          });
+          console.log(this.state.films);
+        });
   }
   // eslint-disable-next-line require-jsdoc
   componentDidMount() {
@@ -24,24 +43,33 @@ class Films extends React.Component {
   // eslint-disable-next-line require-jsdoc
   render() {
     const {isLoaded, films} = this.state;
+    // eslint-disable-next-line max-len
+    // const sortDates =(a, b) => moment(a.release_date, 'YYYY-MM-DD') -moment(b.release_date, 'YYYY-MM-DD');
     return (
       <div className="films">
+        {this.state.loading ?
+              <div className="loader">
+                <img className="loader__image" src={Loader} alt="loader"/>
+              </div> :
+              null
+        }
         <div className="container">
-          <a className="container__left">
-            <img className="arrowLeft" src={arrowYellow} alt="arrowLeft"/>
-          </a>
-          <a className="container__right">
-            <img className="arrowRight" src={arrowYellow} alt="arrowRight"/>
-          </a>
-          {/* eslint-disable-next-line max-len */}
           {!isLoaded ?
               null :
               // eslint-disable-next-line max-len
               <div className="films__list">
-                {/* eslint-disable-next-line max-len */}
-                {films.map((films) =>
-                // eslint-disable-next-line max-len
-                  <div className="films__list--item" key={films.episode_id}>{films.title}</div>
+                {films.map((film) =>
+                // eslint-disable-next-line max-len,react/jsx-key
+                  <a href='#' className="films__list--button">
+                    <img
+                      className="films__list--poster"
+                      src={images[film.episode_id-1]}
+                      alt="poster"
+                    />
+                    <div className="films__list--item" key={film.episode_id}>
+                      {film.title}
+                    </div>
+                  </a>,
                 )}
               </div>
           }
