7234d6a952e56e73a1a7f073951da8973850e909
Jakub Łasecki <s426179@labs.wmi.amu.edu.pl>
2019-11-25 13:11:10 +0100

    Updates

---
+++ precache-manifest.1945193302c704c924142be8b73508c5.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ee595215a53e4356ea3640b54dc4836",
+    "url": "/star-wars-wiki/index.html"
+  },
+  {
+    "revision": "2ba797a5f84e858f36e2",
+    "url": "/star-wars-wiki/static/css/main.34ab8b96.chunk.css"
+  },
+  {
+    "revision": "4cb56c8e978608bf573a",
+    "url": "/star-wars-wiki/static/js/2.6f3817bb.chunk.js"
+  },
+  {
+    "revision": "2ba797a5f84e858f36e2",
+    "url": "/star-wars-wiki/static/js/main.a3b75108.chunk.js"
+  },
+  {
+    "revision": "94b079bf2ab39f75396d",
+    "url": "/star-wars-wiki/static/js/runtime-main.52d59acf.js"
+  },
+  {
+    "revision": "e1dd3c8bcb60c8bc2ecddc0a46017942",
+    "url": "/star-wars-wiki/static/media/1.e1dd3c8b.png"
+  },
+  {
+    "revision": "48df9778a694e02df8bbcced9cf42e62",
+    "url": "/star-wars-wiki/static/media/2.48df9778.jpg"
+  },
+  {
+    "revision": "7ed009af4d1f9cf96bd8517433d27789",
+    "url": "/star-wars-wiki/static/media/3.7ed009af.jpg"
+  },
+  {
+    "revision": "8addba9bbe507705724a0f13a6d57d21",
+    "url": "/star-wars-wiki/static/media/4.8addba9b.png"
+  },
+  {
+    "revision": "4c968f7e33d28d0c60bd0437b9753ce9",
+    "url": "/star-wars-wiki/static/media/5.4c968f7e.png"
+  },
+  {
+    "revision": "dc159e323f7ef7a7bcc3c23b328dba46",
+    "url": "/star-wars-wiki/static/media/6.dc159e32.png"
+  },
+  {
+    "revision": "238579d4509d0080f9a1063dda375331",
+    "url": "/star-wars-wiki/static/media/7.238579d4.jpg"
+  },
+  {
+    "revision": "eaf52cfb2278af92630ecf528eb0ca02",
+    "url": "/star-wars-wiki/static/media/clons.eaf52cfb.png"
+  },
+  {
+    "revision": "e41821dedec25c9c519d139b3223a96a",
+    "url": "/star-wars-wiki/static/media/film-description.e41821de.jpg"
+  },
+  {
+    "revision": "6b051b765f5e485e6be9a26c74c05dfc",
+    "url": "/star-wars-wiki/static/media/loader.6b051b76.png"
+  },
+  {
+    "revision": "5427d0b6a387d5bc75159e47e6d2d79e",
+    "url": "/star-wars-wiki/static/media/people.5427d0b6.png"
+  },
+  {
+    "revision": "0c4218b94f9b4a5bc1ea1a5b4074e9b0",
+    "url": "/star-wars-wiki/static/media/planets.0c4218b9.jpg"
+  },
+  {
+    "revision": "136469fc46abf363bbeef7de2de62fa5",
+    "url": "/star-wars-wiki/static/media/starsBcg.136469fc.png"
+  },
+  {
+    "revision": "0e0f1ce5ee83eb697210e7c31f9a3b28",
+    "url": "/star-wars-wiki/static/media/test.0e0f1ce5.png"
+  }
+]);
